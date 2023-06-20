feffadd27784ca9255f87f2218799f34495fcbff
Rowsen <bayramovw0@gmail.com>
2023-06-21 02:09:07 +0500

    created serializers to Branch, Department & Worker

---
+++ branch/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Branch, Department
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = "__all__"
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = "__all__"
