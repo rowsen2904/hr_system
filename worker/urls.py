c33f6dc135b465c33882bdd4b2325604d2d04f24
Rowsen <bayramovw0@gmail.com>
2023-06-21 18:36:26 +0300

    kwargs testing

--- worker/urls.py
+++ worker/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.WorkerList.as_view())
+    path("", views.WorkerList.as_view()),
+    path("test/<str:pk>", views.WorkerBranch.as_view()),
 ]
--- worker/views.py
+++ worker/views.py
@@ -5,8 +5,22 @@ from .models import Worker
 from .paginations import WorkerPagination
 from .serializers import WorkerSerializer
 
+
 class WorkerList(generics.ListAPIView):
     queryset = Worker.objects.all()
     pagination_class = WorkerPagination
     serializer_class = WorkerSerializer
     permission_classes = [IsAuthenticated]
+
+
+class WorkerBranch(generics.ListAPIView):
+    pagination_class = WorkerPagination
+    serializer_class = WorkerSerializer
+    # permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        print(pk)
+        return Worker.objects.all()
+        
+    
